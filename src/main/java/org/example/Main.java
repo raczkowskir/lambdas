6ca92bb80b6e183137e883482a78d1a1ecd5126c
Rafal Raczkowski <rafal.raczkowski@nokia.com>
2023-03-14 16:57:52 +0100

    Using lambda as Consumer.
    Signed-off-by: Rafal Raczkowski

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,22 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-    List<String> numbers = Arrays.asList("one", "Two", "there", "four");
+        List<String> numbers = Arrays.asList("one", "Two", "there", "four");
 
-    Comparator<String> comparator =  (a, b) -> a.compareToIgnoreCase(b);
-    Consumer<String> consumer = x -> System.out.println(x);
-    Predicate<String> predicate = x -> x != "one";
+        Comparator<String> comparator = (a, b) -> a.compareToIgnoreCase(b);
+        Consumer<String> consumer = x -> System.out.println(x);
+        Predicate<String> predicate = x -> x != "one";
 
-    Stream stream = Stream.concat(numbers.stream().filter(predicate), numbers.stream().sorted(comparator));
-    stream.forEach(consumer);
+        Stream stream = Stream.concat(numbers.stream().filter(predicate), numbers.stream().sorted(comparator));
+//    stream.forEach(consumer);
+
+        List<String> things = new ArrayList<>();
+        things.add("brick");
+        things.addAll(Arrays.asList("block", "rock"));
+        things.forEach(x -> printWithSuffix(""));
+    }
+
+    public static void printWithSuffix(String value) {
+        System.out.println(value + " is super!");
     }
 }
